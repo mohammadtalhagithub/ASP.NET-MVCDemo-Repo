e9b6e98700b00c45db2ff24fe24b25b091b61997
mohammadtalhagithub <md.talha226@gmail.com>
2023-05-23 20:32:39 +0530

    Added form using (Html.BeginForm())

---
+++ StronglyTypedHtmlHelpers_Project/Models/SignUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StronglyTypedHtmlHelpers_Project.Models
+{
+    public class SignUp
+    {
+        public int UserID { get; set; }
+
+        public string UserName { get; set; }
+
+        public string UserPassword { get; set; }
+
+        public string UserGender { get; set; }
+
+        public string UserEmail { get; set; }
+
+        public string UserComment { get; set; }
+
+        public string UserPage { get; set; }
+    }
+}
